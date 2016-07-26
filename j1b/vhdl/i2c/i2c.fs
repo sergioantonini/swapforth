689ed7fba6c0f9b94efdf617718fd20e650f0160
Wojciech M. Zabolotny <wzab01@gmail.com>
2016-07-27 01:07:32 +0200

    Added implemetation of basic I2C routines

---
+++ j1b/vhdl/i2c/i2c.fs
@@ -0,0 +1,69 @@
+16 base !
+6100 constant I2C_REGS 
+a base !
+
+: err_halt 
+  dup .
+  throw
+;
+: i2c_init
+  200 I2C_REGS io!
+  0 I2C_REGS 1 + io!
+  128 I2C_REGS 2 + io!
+;
+
+\ i2c_wr1 writes a single byte 
+: i2c_wr1 ( dta addr -- )
+  2* I2C_REGS 3 + io! \ set address
+  128 16 or I2C_REGS 4 + io! \ CMD: STA+WR
+  \ Wait for ACK
+  begin 
+    I2C_REGS 4 + io@
+    dup 2 and while
+    drop 
+  repeat
+  128 and if
+     \ NACK in address
+     133 err_halt then
+  I2C_REGS 3 + io!
+  64 16 or
+  I2C_REGS 4 + io!
+  begin 
+    I2C_REGS 4 + io@
+    dup 2 and while
+    drop 
+  repeat
+  128 and if
+     \ NACK in data
+     134 err_halt then
+;    
+
+: i2c_rd1 ( addr -- data )
+  2* 1+ I2C_REGS 3 + io! \ set address
+  128 16 or I2C_REGS 4 + io! \ CMD: STA+WR
+  begin 
+    I2C_REGS 4 + io@
+    dup 2 and while
+    drop
+  repeat
+  128 and if
+     \ NACK in address
+     133 err_halt then
+  64 32 or 8 or I2C_REGS 4 + io!
+  begin 
+    I2C_REGS 4 + io@
+    dup 2 and while
+    drop
+  repeat
+  drop
+  I2C_REGS 3 + io@  
+;
+    
+\ i2c_wr writes multiple bytes stored in a byte array.
+\ The first byte contains the length of the array.
+\ Next bytes contain the data to be sent
+
+: i2c_wr ( dtaptr addr -- )
+;  
+
+     
